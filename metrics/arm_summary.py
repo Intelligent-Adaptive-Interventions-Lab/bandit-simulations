5e41340afe791c263e27bcf626f3e08204f80425
VictorS67 <185000048@qq.com>
2022-08-12 09:12:48 -0400

    fix issues in summary

--- metrics/arm_summary.py
+++ metrics/arm_summary.py
@@ -5,14 +5,12 @@ from typing import List
 
 
 def arm_summary(simulation_dfs: List[pd.DataFrame], reward_name: str) -> pd.DataFrame:
-    trail_column = ["trail_number"] + simulation_dfs[0].columns
-    
-    all_simulation_df = pd.DataFrame(columns=trail_column)
-    
+    all_simulation_df = []
+
     for i in range(len(simulation_dfs)):
-        simulation_dfs[0]["trail_number"] = i
-        all_simulation_df = pd.concat([all_simulation_df, simulation_dfs[i]])
-    
-    arm_group = all_simulation_df.groupby(by=["trail_number", "arm"]).agg({reward_name: ['min', 'max', 'mean', 'std', 'sem', 'count'], 'arm' : ['count']})
+        simulation_dfs[i]["trail_number"] = i
+        all_simulation_df.append(simulation_dfs[i])
+
+    arm_group = pd.concat(all_simulation_df).groupby(by=["trail_number", "arm"]).agg({reward_name: ['min', 'max', 'mean', 'std', 'sem', 'count'], 'arm' : ['count']})
     
     return arm_group
